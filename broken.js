cb03729a41780b5056f5c64ca8e0592570beb5e0
Serhatbilge93 <serhat_bilge@hotmail.com>
2025-05-28 15:14:55 +0200

    Fix JS errors to pass Super-Linter

--- broken.js
+++ broken.js
@@ -1,4 +1,8 @@
-let x = 5
-let y = 7
-function double(x) { return x*2 )
-console.print("Dubbelt av 5 är " + double(x));
+let x = 5;
+let y = 7;
+
+function double(x) {
+  return x * 2;
+}
+
+console.log("Dubbelt av 5 är " + double(x));
